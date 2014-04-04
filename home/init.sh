bb20d0acebd5f6bd8624e43cbfef552d1e316798
yoccola <hackshon@gmail.com>
2014-04-04 17:38:03 +0900

    初期設定用のスクリプト追加

---
+++ home/init.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+ln -fs dotfiles/home/.bashrc ~/
+ln -fs dotfiles/home/.zshrc ~/
+ln -fs dotfiles/home/.zshrc_alias ~/
+ln -fs dotfiles/home/.vimrc ~/
+ln -fs dotfiles/home/.tmux.conf ~/
