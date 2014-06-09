91a1383dd148a1124ee472055367c1cfdaf45af4
yoccola <hackshon@gmail.com>
2014-06-09 16:34:53 +0900

    Add shell script for subversion

---
+++ home/bin/svn-display_file_lists_by_author_and_revision.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+while getopts a:r: OPT
+do
+  case $OPT in
+    "a" ) FLG_A="TRUE" ; AUTHOR="$OPTARG";;
+    "r" ) FLG_R="TRUE" ; REV="$OPTARG";;
+      * ) echo "Usage: $CMDNAME [-a AUTHOR_NAME] [-r FROM_REVISION]" 1>&2
+          exit 1 ;;
+  esac
+done
+
+if [ "$FLG_A" != "TRUE" ]; then
+  echo "Usage: $CMDNAME [-a AUTHOR_NAME] [-r FROM_REVISION]"
+fi
+
+
+svn log -r{$REV}:HEAD -v | sed -n '/$AUTHOR/,/-----$/ p' | grep '^ ' | awk '{ print $2 }' | sort -u
+
