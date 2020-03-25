62c88bdce6c56198c126ac529375612b75a06950
Hitoshi Imai <hitoshi.imai@imjp.co.jp>
2020-03-25 19:13:00 +0900

    fish起動時にAWS用の環境変数を設定するように修正

--- home/.config/fish/config.fish
+++ home/.config/fish/config.fish
@@ -4,7 +4,13 @@ if status --is-login
 end
 
 # Variables
-set -gx AWS_REGION "ap-northeast-1"
+## AWS
+if test -x /usr/local/bin/aws
+  set -gx AWS_REGION "ap-northeast-1"
+  set -gx AWS_ACCESS_KEY_ID "(aws configure get aws_access_key_id)"
+  set -gx AWS_SECRET_ACCESS_KEY "(aws configure get aws_secret_access_key)"
+end
+
 
 # aliases
 alias dc='docker-compose'
